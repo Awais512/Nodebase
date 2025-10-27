68c98dc469d3abc41bb0cf5d399194d64e10d17d
awais512 <raza.awais25@gmail.com>
2025-10-27 21:09:44 +0000

    Payments setup with polar

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,5 +1,7 @@
+import { checkout, polar, portal } from "@polar-sh/better-auth";
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
+import { polarclient } from "./polar";
 
 import prisma from "@/lib/db";
 
@@ -11,4 +13,23 @@ export const auth = betterAuth({
     enabled: true,
     autoSignIn: true,
   },
+  plugins: [
+    polar({
+      client: polarclient,
+      createCustomerOnSignUp: true,
+      use: [
+        checkout({
+          products: [
+            {
+              productId: "90bebc37-ef4f-43f0-86c4-d9ce7ddc7a16",
+              slug: "pro",
+            },
+          ],
+          successUrl: process.env.POLAR_SUCCESS_URL,
+          authenticatedUsersOnly: true,
+        }),
+        portal(),
+      ],
+    }),
+  ],
 });
